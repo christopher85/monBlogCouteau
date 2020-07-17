8a89bbbb411cd235bffe77fd260e3ee8abf9a0e6
debien <christopherdebien@gmail.com>
2020-07-17 09:01:30 +0200

    modif tous

--- app.js
+++ app.js
@@ -4,26 +4,36 @@ const Handlebars = require('handlebars');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const exphds = require('express-handlebars');
-const {allowInsecurePrototypeAccess,} = require('@handlebars/allow-prototype-access');
+const {
+    allowInsecurePrototypeAccess,
+} = require('@handlebars/allow-prototype-access');
 const methodOverride = require('method-override');
 
 
 const app = express();
 
 // Handlebars
-app.engine("hbs", exphds({defaultLayout: "main",extname: "hbs",
-handlebars: allowInsecurePrototypeAccess(Handlebars)}));
+app.engine("hbs", exphds({
+    defaultLayout: "main",
+    extname: "hbs",
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+}));
 
 //method override
 app.use(methodOverride("_method"));
 
 //MongoDB
-mongoose.connect("mongodb://localhost:27017/boutiqueGame", {useNewUrlParser: true,useUnifiedTopology: true,});
+mongoose.connect("mongodb://localhost:27017/monBlogCouteau", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
-app.set('view engine','hbs');
+app.set('view engine', 'hbs');
 
 //body parser 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 //static
 app.use(express.static('public'));
@@ -33,10 +43,11 @@ app.use(express.static('public'));
 const productSchema = new mongoose.Schema({
     title: String,
     content: String,
+    category: String,
     cover: {
         name: String,
         originalName: String,
-        path:String,
+        path: String,
         urlSharp: String,
         createAt: Date
 
@@ -45,45 +56,99 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model("product", productSchema);
 
-//route 
-app.route('/acceuil')
-.get((req, res) =>{
-    Product.find((err,product) => {
-        if(!err) {
-            res.render("acceuil",{
-                produit: product
-            })
-        }else{
-            res.send(err)
-        }
+//route GET
+
+app.route('/get')
+    .get((req, res) => {
+        Product.find((err, product) => {
+            if (!err) {
+                res.render("get", {
+                product: product
+                })
+            } else {
+                res.send(err)
+            }
+        })
+    })
+    .post()
+
+//METHODE POST
+
+app.route('/post')
+    .get((req, res) => {
+        res.render("post")
+
+    })
+    .post((req, res) => {
+        const newProduct = new Product({
+            title: req.body.title,
+            content: req.body.content,
+            category: req.body.category,
+            cover: req.body.cover,
+        });
+        newProduct.save(function (err) {
+            if (!err) {
+                res.send("save ok !!!")
+            } else {
+                res.send(err)
+            }
+        })
+
     })
 
-})
-.post((req, res) =>{
-    const newProduct = new Product({
-        title: req.body.title,
-        content: req.body.content,
-        price: req.body.price,
-    });
-    newProduct.save(function(err){
-        if(!err){
-            res.send("save ok !!!")
-        }else{
-            res.send(err)
-        }
+//MISE A JOUR
+
+ app.route('/put')
+    .get((req, res) => {
+        res.render("put")
     })
+    .put(function(req, res){
+        Product.update(
+            //condition
+            {_id: req.params.id},
+            //update
+            {
+                title: req.body.title,
+                content: req.body.content,
+            },
+            //option
+            {multi:true},
+            //execute
+            function(err){
+                if(!err){
+                    res.send("Update OK!!")
+                }else{
+                    res.send(err)
+                }
+            }
     
-})
+        )
+    })
 
 
 
+//DELETE
+
+app.route('/delete')
+    .get((req, res) => {
+        res.render("delete")
+
+    })
+    .delete(function(req,res){
+        Product.deleteOne(
+            {_id:req.params.id},
+            function(err){
+                if(!err){
+                    res.send("product delete")
+                }else {
+                    res.send(err)
+                }
+            }
+        )
+    })
 
 
-
-
-
-app.listen(1985, function(){
+app.listen(1985, function () {
     console.log(`écoute le port 1985,  lancé à:${new Date().toLocaleString()}`);
-    
-})
 
+})
